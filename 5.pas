87b371905cb12fcfaa9bc817acfbb70874c676ad
Artiick <117628635+artemiozy@users.noreply.github.com>
2022-12-12 04:07:26 +0300

    Add files via upload

---
+++ 5.pas
@@ -0,0 +1,35 @@
+﻿program пять;
+uses GraphABC;
+var x1,x2,y1,y2: integer;
+begin
+  Circle(50,50,10);
+   FloodFill(50,50,clBlue);
+   begin
+  Circle(110,100,20);
+   FloodFill(100,100,clFuchsia);
+   end;
+   begin
+  Circle(170,150,30);
+   FloodFill(150,150,clRed);
+   end;
+   begin
+  Circle(220,200,40);
+   FloodFill(200,200,clOrange);
+   end;
+   begin
+  Circle(270,250,50);
+   FloodFill(250,250,clBlack);
+   end;
+   begin
+  Circle(320,300,60);
+   FloodFill(300,300,clYellow);
+   end;
+   begin
+  Circle(370,350,70);
+   FloodFill(350,350,clwhite);
+   end;
+    begin
+  Circle(420,400,80);
+   FloodFill(400,400,clGreen);
+   end;
+   end.
